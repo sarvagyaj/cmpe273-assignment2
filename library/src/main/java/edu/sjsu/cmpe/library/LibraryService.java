f66dfe137862cefef07fddec92ad19697c6d95f4
sarvagyaj <sarvagyaj@gmail.com>
2013-11-05 08:14:09 -0800

    Chaning the location of dropwiz-jobs and added mustache code for ui

--- library/src/main/java/edu/sjsu/cmpe/library/LibraryService.java
+++ library/src/main/java/edu/sjsu/cmpe/library/LibraryService.java
@@ -4,6 +4,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.assets.AssetsBundle;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 import com.yammer.dropwizard.views.ViewBundle;
@@ -27,6 +28,7 @@ public class LibraryService extends Service<LibraryServiceConfiguration> {
 	public void initialize(Bootstrap<LibraryServiceConfiguration> bootstrap) {
 		bootstrap.setName("library-service");
 		bootstrap.addBundle(new ViewBundle());
+		bootstrap.addBundle(new AssetsBundle());
 	}
 
 	@Override
--- library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
+++ library/src/main/java/edu/sjsu/cmpe/library/ui/views/HomeView.java
@@ -1,18 +1,20 @@
 package edu.sjsu.cmpe.library.ui.views;
 
+import java.util.List;
+
 import com.yammer.dropwizard.views.View;
 
 import edu.sjsu.cmpe.library.domain.Book;
 
 public class HomeView extends View {
-	private final Book book;
+	private final List<Book> books;
 
-	public HomeView(Book book) {
+	public HomeView(List<Book> books) {
 		super("home.mustache");
-		this.book = book;
+		this.books = books;
 	}
 
-	public Book getBook() {
-		return book;
+	public List<Book> getBooks() {
+		return books;
 	}
 }
