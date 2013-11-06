960bd916bf7881bf6d297acf47cb6d00672aec6c
sarvagyaj <sarvagyaj@gmail.com>
2013-11-06 21:05:26 +0000

    modified js for lost status

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,15 +1,28 @@
+$(function(){
+	$('tr').each(function(){
+	if ($(this).find('.status').text() == "lost")
+		$(this).find('button').prop("disabled", "true");
+});
+});
+
 
 $(":button").click(function() {
-    var isbn = this.id;
-    alert('About to report lost on ISBN ' + isbn);    
+    var btn = $(this),
+	tr = btn.closest('tr'),
+    isbn = tr.attr("data-id");
+    console.log('About to report lost on ISBN ' + isbn);    
+
     
     $.ajax({
         url: '/library/v1/books/'+isbn+'?status=lost',
         type: 'PUT',
         contentType: 'application/json',
-        success: function(result) {     
+        success: function(result) {
+		btn.prop('disabled', 'true');
+		tr.find('.status').html('lost');
+     
         	//$('#'+isbn).prop('disabled', true);
-        	location.reload();
+        	//location.reload();
         	
         	//jQuery('#'+isbn).prop("disabled", true);
         	//alert('Successful hit');
@@ -22,5 +35,4 @@ $(":button").click(function() {
         	//$(this).hide().delay(30000);
         }
     })
-    $(this).prop('disabled', true);
 });
